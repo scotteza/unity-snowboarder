7e754a0f20cf49abf66b1f62b2ff466a62d119eb
Scott Edwards <scotteza@gmail.com>
2022-06-28 21:26:48 +0100

    Add torque input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _torqueAmount = 3f;
+
+    private Rigidbody2D _rigidBody;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidBody.AddTorque(_torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _rigidBody.AddTorque(-_torqueAmount);
+        }
+    }
+}
