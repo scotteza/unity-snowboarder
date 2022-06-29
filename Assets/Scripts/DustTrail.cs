1af66719d53309ea1c984e633aa67f1cd25a3ecb
Scott Edwards <scotteza@gmail.com>
2022-06-29 18:59:00 +0100

    Disable dust trail if player dies

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -1,13 +1,26 @@
+using System;
 using UnityEngine;
 
 public class DustTrail : MonoBehaviour
 {
     [SerializeField] private ParticleSystem dustTrailParticleSystem;
+    
+    private GameState _gameState;
 
     private const string GroundTag = "Ground";
 
+    private void Start()
+    {
+        _gameState = FindObjectOfType<GameState>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (!_gameState.IsPlayerAlive())
+        {
+            return;
+        }
+        
         if (collision.gameObject.CompareTag(GroundTag))
         {
             dustTrailParticleSystem.Play();
