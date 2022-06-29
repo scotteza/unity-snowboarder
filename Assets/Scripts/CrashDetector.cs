5afb2be2fac50cfcbdd7674e37ecc0a649d0033f
Scott Edwards <scotteza@gmail.com>
2022-06-29 15:34:11 +0100

    Pause before reloading scene on crash or win

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // ReSharper disable CheckNamespace
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable SuggestBaseTypeForParameter
 // ReSharper disable UnusedMember.Global
 // ReSharper disable UnusedMember.Local
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private float _secondsToReloadSceneAfterCrash = 0.5f;
     private const string GroundTag = "Ground";
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,6 +20,11 @@ public class CrashDetector : MonoBehaviour
             return;
         }
 
+        Invoke(nameof(ReloadScene), _secondsToReloadSceneAfterCrash);
+    }
+
+    private void ReloadScene()
+    {
         SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // ReSharper disable CheckNamespace
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable SuggestBaseTypeForParameter
 // ReSharper disable UnusedMember.Global
 // ReSharper disable UnusedMember.Local
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private float _secondsToReloadSceneAfterFinish = 0.5f;
+
     private const string PlayerTag = "Player";
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,6 +21,11 @@ public class FinishLine : MonoBehaviour
             return;
         }
 
+        Invoke(nameof(ReloadScene), _secondsToReloadSceneAfterFinish);
+    }
+
+    private void ReloadScene()
+    {
         SceneManager.LoadScene(0);
     }
 }
