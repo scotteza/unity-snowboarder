0fff1c74a9cb662643939b96ccbafee129876e77
Scott Edwards <scotteza@gmail.com>
2022-06-29 18:58:26 +0100

    Kill player if crash is detected

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,12 +8,14 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] private AudioClip crashSoundEffect;
 
     private AudioSource _audioSource;
+    private GameState _gameState;
 
     private const string GroundTag = "Ground";
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        _gameState = FindObjectOfType<GameState>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,6 +27,7 @@ public class CrashDetector : MonoBehaviour
 
         crashParticleSystem.Play();
         _audioSource.PlayOneShot(crashSoundEffect);
+        _gameState.KillPlayer();
         Invoke(nameof(ReloadScene), secondsToReloadSceneAfterCrash);
     }
 
